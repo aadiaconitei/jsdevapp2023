f23ce56339dc38081d412c18787858f30917066d
aadiaconitei <aadiaconitei@yahoo.com>
2023-06-22 20:56:23 +0300

    blog

--- reactBlog/frontend/src/pages/Home.jsx
+++ reactBlog/frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from "react";
 import Header from '../components/Header'
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 import configData from "../config.json";
 export default function Home() {
   const [posts, setPosts]  = useState([{}]);
@@ -22,25 +22,26 @@ export default function Home() {
   return (
    <>
    <Header />
-   {posts.map(data => 
-      <div key={data.id}>
-          <p>{data.titlu}</p>
-      </div>
-      )}
+   
    <div className="container">
     <div className="row mt-4">
       <h1>Ultimele articole</h1>
-      <div className="col">
+
+      {posts.map(data => 
+      <div key={data.id} className="col">
         <div className="card mb-4">
-            <a href="#!"><img className="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+            <a href="#!"><img className="card-img-top" src={data.poza} alt="..." /></a>
             <div className="card-body">
                 <div className="small text-muted">January 1, 2023</div>
-                <h2 className="card-title h4">Post Title</h2>
-                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                <a className="btn btn-primary" href="#!">Read more →</a>
+                <h2 className="card-title h4">{data.titlu}</h2>
+                <p className="card-text">{data.continut}</p>
+                <Link to={'/post/'+data.id} className='btn btn-primary'>Read more</Link>
+                {/* <a className="btn btn-primary" href="#!">Read more →</a> */}
+
             </div>
         </div>
       </div>
+      )}
     </div>                              
    </div>
    </>
