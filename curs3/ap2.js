1482ff513df556c64cc55aaf0079c6d52cf96b89
aadiaconitei <aadiaconitei@yahoo.com>
2023-06-22 20:13:27 +0300

    blog

---
+++ curs3/ap2.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+
+let vm = createApp({
+    data() {
+        return {
+            fname: "Ion",
+            lname: "Popescu",
+            address: "Brasov",
+            message: "Imi place JavaScript",
+           
+        }
+    },
+    methods: {
+        myName: function () {
+            // return "Eu ma numesc " + this.fname + " " + this.lname + " si sunt din " + this.address;
+            return `Eu ma numesc ${this.fname} ${this.lname} 
+            si sunt din  ${this.address}`;
+        }
+    }
+}).mount('#app');
---
+++ reactBlog/reactblog.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 20, 2023 at 01:11 PM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reactblog`
+--
+
+-- --------------------------------------------------------
+§
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
